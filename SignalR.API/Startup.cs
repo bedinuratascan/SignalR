2070f637eae583f8da97057867c38f0b195973d0
bedinuratascan <bedinuratascan@gmail.com>
2020-10-24 21:59:16 +0300

    Created SignalR Server

--- SignalR.API/Startup.cs
+++ SignalR.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SignalR.API.Hubs;
 
 namespace SignalR.API
 {
@@ -26,6 +27,7 @@ namespace SignalR.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,7 @@ namespace SignalR.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<MyHub>("/myhub");
             });
         }
     }
